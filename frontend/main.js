772791c3be9c8e99ff781d7fa9a1e3dbb8fe115c
Lucca <luccaviniciusmoreira@gmail.com>
2026-01-19 21:47:49 -0300

    update no main

--- frontend/main.js
+++ frontend/main.js
@@ -2,7 +2,6 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import Login from './modules/Login'; 
 import CadastraContato from './modules/CadastraContato';
-// import './assets/css/style.css';
 
 const login = new Login('.form-login');
 const cadastro = new Login('.form-cadastro');
